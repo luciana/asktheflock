0cb6b83707f843e90801984a4f1c5bbb46c05640
Luciana Bruscino <luciana.bruscino@gmail.com>
2023-03-01 21:44:28 -0500

    shorten ulrs

--- src/Services/shortenURL.js
+++ src/Services/shortenURL.js
@@ -5,7 +5,7 @@ const shortenURL = async (input) => {
      return await axios(
       `https://api.shrtco.de/v2/shorten?url=${input}`
     ).then((response) =>{
-        console.log("calling shorten url ", response.data.result.full_short_link);
+        console.log("calling shorten url ", response.data.result);
         return response.data.result.full_short_link;
     }).catch((error) => {
         console.error("Error shortening URL Service", error);
