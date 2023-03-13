43993a0ebaec83c44c3eeb617af7b25d24001da4
Luciana Bruscino <luciana.bruscino@gmail.com>
2023-03-13 14:05:56 -0400

    removed s3 hosting and commented out service worker

--- src/index.js
+++ src/index.js
@@ -61,25 +61,25 @@ root.render(
 reportWebVitals();
 
 
-if ("serviceWorker" in navigator) {
-  const firebaseConfig = encodeURIComponent(
-    JSON.stringify({
-      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    })
-  );
-  navigator.serviceWorker
-    .register(
-      `../../firebase-messaging-sw.js?firebaseConfig=${firebaseConfig}`
-    )
-    .then(function (registration) {
-      //console.log("Registration successful, scope is:", registration.scope);
-    })
-    .catch(function (err) {
-      console.error("Service worker registration failed, error:", err);
-    });
-  }
+// if ("serviceWorker" in navigator) {
+//   const firebaseConfig = encodeURIComponent(
+//     JSON.stringify({
+//       apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//       authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//       projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//       storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//       messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//       appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//     })
+//   );
+//   navigator.serviceWorker
+//     .register(
+//       `../../firebase-messaging-sw.js?firebaseConfig=${firebaseConfig}`
+//     )
+//     .then(function (registration) {
+//       //console.log("Registration successful, scope is:", registration.scope);
+//     })
+//     .catch(function (err) {
+//       console.error("Service worker registration failed, error:", err);
+//     });
+//   }
