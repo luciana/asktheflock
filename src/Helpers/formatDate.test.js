f231f5caddca9ffa3648a32ab0fbd28e5fd22d76
Chad Edwards <chad@saw.dev>
2023-03-18 18:43:47 -0400

    Simplified format date to be able to use any international format

--- src/Helpers/formatDate.test.js
+++ src/Helpers/formatDate.test.js
@@ -1,42 +1,24 @@
-import FormatDate from './formatDate';
+import FormatDate from './FormatDate';
 
 describe('FormatDate', () => {
-  describe('Show', () => {
-    it('returns the correct formatted date for pt-BR locale', () => {
-      const date = '2021-03-18';
-      const locale = 'pt-BR';
-      const formattedDate = FormatDate.Show(date, locale);
-      expect(formattedDate).toBe('18/03/2021');
-    });
-
-    it('returns the correct formatted date for other locales', () => {
-      const date = '2021-03-18';
-      const locale = 'en-US';
-      const formattedDate = FormatDate.Show(date, locale);
-      expect(formattedDate).toBe('2021-03-18');
-    });
+  it('formats the date correctly for the en-US locale', () => {
+    const date = '2022-04-18';
+    const locale = 'en-US';
+    const formattedDate = FormatDate(date, locale);
+    expect(formattedDate).toBe('04/18/2022');
   });
 
-  describe('Format', () => {
-    it('returns the correctly formatted date for pt-BR locale', () => {
-      const date = '18032021';
-      const locale = 'pt-BR';
-      const formattedDate = FormatDate.Format(date, locale);
-      expect(formattedDate).toBe('18/03/2021');
-    });
+  it('formats the date correctly for the fr-FR locale', () => {
+    const date = '2022-04-18';
+    const locale = 'fr-FR';
+    const formattedDate = FormatDate(date, locale);
+    expect(formattedDate).toBe('18/04/2022');
+  });
 
-    it('returns the correctly formatted date for other locales', () => {
-      const date = '20210318';
-      const locale = 'en-US';
-      const formattedDate = FormatDate.Format(date, locale);
-      expect(formattedDate).toBe('2021-03-18');
-    });
-
-    it('returns the original date if input is not provided', () => {
-      const date = null;
-      const locale = 'en-US';
-      const formattedDate = FormatDate.Format(date, locale);
-      expect(formattedDate).toBeNull();
-    });
+  it('formats the date correctly for the pt-BR locale', () => {
+    const date = '2022-04-18';
+    const locale = 'pt-BR';
+    const formattedDate = FormatDate(date, locale);
+    expect(formattedDate).toBe('18/04/2022');
   });
 });
