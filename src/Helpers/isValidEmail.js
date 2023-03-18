50e07b425a5cadaf38ac385bd14ffea35b6ad2d6
Chad Edwards <chad@saw.dev>
2023-03-18 18:10:23 -0400

    fixed email validation.. not sure why with comments messed it up

--- src/Helpers/isValidEmail.js
+++ src/Helpers/isValidEmail.js
@@ -1,21 +1,12 @@
 const isValidEmail = (email) => {
   const re = new RegExp(
-    // ^: start of the line
-    '^' +
-    // [a-zA-Z0-9._%+-]+: one or more alphanumeric characters, '.', '_', '%', '+', or '-'
-    '[a-zA-Z0-9._%+-]+' +
-    // @: the '@' symbol
-    '@' +
-    // [a-zA-Z0-9.-]+: one or more alphanumeric characters, '.', or '-'
-    '[a-zA-Z0-9.-]+' +
-    // \.: a literal dot '.'
-    '\\.' +
-    // [a-zA-Z]{2,}: two or more alphabetic characters for the top-level domain
-    '[a-zA-Z]{2,}' +
-    // $: end of the line
-    '$'
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})$/
+
   );
-  return re.test(email);
+  const result = re.test(email);
+  console.log(`${email}: ${result}`);
+  return result;
 };
 
 export default isValidEmail;
+
