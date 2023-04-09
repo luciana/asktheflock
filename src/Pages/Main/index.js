833a02f472d8249e6230fc840b7dac0d86276847
Luciana Bruscino <luciana.bruscino@gmail.com>
2023-04-09 14:27:40 -0400

    updates to profile missing messages

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -12,22 +12,21 @@ function Main() {
   const { user } = state;
   const [alert, setAlert] = useState({});
 
-  console.log("user info ", user);
   const showProfileMissingMessage = () => {
     if (user.userTag && (!user.gender || user.gender === "") && user.address && user.birthdate){
-      console.log("only gender is missing");
+     // console.log("only gender is missing");
       setAlert({ type: "warning", text: LANGUAGES[user.locale].Profile.OnlyGenderMissing, link: "/Profile"});  
     }else if (user.userTag && user.gender && (!user.address || user.address === "") && user.birthdate){
-      console.log("only address is missing");
+     // console.log("only address is missing");
       setAlert({ type: "warning", text: LANGUAGES[user.locale].Profile.OnlyAddressMissing, link: "/Profile"});  
     }else if (user.userTag && user.gender && user.address && (!user.birthdate || user.birthdate === "")){
-      console.log("only birthday is missing");
+     // console.log("only birthday is missing");
       setAlert({ type: "warning", text: LANGUAGES[user.locale].Profile.OnlyBirthdayMissing, link: "/Profile"});  
     }else if ((!user.userTag || user.userTag === "") && user.gender && user.address && user.birthdate){
-      console.log("only expert Tag is missing");
+     // console.log("only expert Tag is missing");
       setAlert({ type: "warning", text: LANGUAGES[user.locale].Profile.OnlyExpertTagMissing, link: "/Profile"});  
-    }else{
-      console.log("generic profile missing message");
+    }else if(!user.userTag || !user.gender || !user.address || !user.birthdate){
+     // console.log("generic profile missing message");
       setAlert({ type: "warning", text: LANGUAGES[user.locale].Profile.CompleteProfile, link: "/Profile"});  
     }
   }
